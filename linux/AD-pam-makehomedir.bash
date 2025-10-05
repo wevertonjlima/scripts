39705840ef80189bab05587ac32b8954a1841fef
Weverton Lima <44751213+wevertonjlima@users.noreply.github.com>
2025-10-05 01:33:49 -0300

    Update and rename AD-pam-makehomedir-function.bash to AD-pam-makehomedir.bash

--- linux/AD-pam-makehomedir.bash
+++ linux/AD-pam-makehomedir.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # ============================================================================
-# Nome do Arquivo: AD-pam-makehomedir-function.bash
+# Nome do Arquivo: AD-pam-makehomedir.bash
 # IA Generated: Sim (ChatGPT - OpenAI)
 # Data de Geração: 03/10/2025
 # ============================================================================
